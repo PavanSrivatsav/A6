dece67922cad05d90942f242baee0a2935ac4dac
PavanSrivatsav <pavansrivatsav96@gmail.com>
2018-09-05 07:07:25 +0530

    angular-hello-world

---
+++ angular-hello-world/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { UserItemComponent } from './user-item/user-item.component';
+import { UserItemsComponent } from './user-items/user-items.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HelloWorldComponent,
+    UserItemComponent,
+    UserItemsComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
