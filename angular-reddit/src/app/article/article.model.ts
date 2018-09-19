c07f69f74416c846770b91fefbdb0a8404701966
PavanSrivatsav <pavansrivatsav96@gmail.com>
2018-09-19 06:56:21 +0530

    resolved 'Law of Demeter'

--- angular-reddit/src/app/article/article.model.ts
+++ angular-reddit/src/app/article/article.model.ts
@@ -10,4 +10,12 @@ export class Article {
         this.votes = vote || 0; // if vote are given takes vote value or else make it as 0
     }
 
+    voteUp(): void {
+        this.votes += 1;
+    }
+
+    voteDown(): void {
+        this.votes -= 1;
+    }
+
 }
