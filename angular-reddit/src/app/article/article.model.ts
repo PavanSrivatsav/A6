584107f335f1b5c61acc70af38853db667cf8b9b
PavanSrivatsav <pavansrivatsav96@gmail.com>
2018-09-21 07:48:08 +0530

    get domain without violating law of demeter

--- angular-reddit/src/app/article/article.model.ts
+++ angular-reddit/src/app/article/article.model.ts
@@ -10,6 +10,15 @@ export class Article {
         this.votes = vote || 0; // if vote are given takes vote value or else make it as 0
     }
 
+
+    getDomain(): string {
+
+        const url = this.link;
+        const domain = url.split("//")[1];
+
+        return domain;
+    }
+
     voteUp(): void {
         this.votes += 1;
     }
